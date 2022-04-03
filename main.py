ac5b2e982432f9652fbdb955334eb333faec2e5f
M-1lan <milan.lefort@icloud.com>
2022-04-03 17:27:33 +0200

    Create game basic code

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+pygame.init()
+
+# Création de la fenêtre de jeu
+
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Petit Jardin")
+
+# Boucle du jeu
+
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+pygame.quit()
