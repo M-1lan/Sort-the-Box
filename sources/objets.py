f96bd14cafddb4b3c85c36f9922b5e5ea0f03bbc
M-1lan <milan.lefort@icloud.com>
2022-04-13 10:32:19 +0200

    Clarified the interaction message

--- sources/objets.py
+++ sources/objets.py
@@ -1,30 +1,30 @@
 import pygame
 
 class Objet(pygame.sprite.Sprite):
-	def __init__(self, allow_interact:bool, allow_overlay:bool)->object:
-		super().__init__()
-		self.allow_interact = allow_interact
-		self.allow_overlay = allow_overlay
+    def __init__(self, allow_interact:bool, allow_overlay:bool)->object:
+        super().__init__()
+        self.allow_interact = allow_interact
+        self.allow_overlay = allow_overlay
 
 
 class ObjetVide(Objet):
-	def __init__(self):
-		super().__init__(False, True)
+    def __init__(self):
+        super().__init__(False, True)
 
 
 class Interactable(Objet):
-	def __init__(self, name):
-		super().__init__(True, False)
-		self.name = name
+    def __init__(self, name):
+        super().__init__(True, False)
+        self.name = name
 
-	def interaction(self, personnage):
-		print("{} ME TOUCHE".format(personnage))
+    def interaction(self, personnage):
+        print("{} interagit avec moi.".format(personnage))
 
 
 class Bloquant(Objet):
-	def __init__(self):
-		super().__init__(False, False)
+    def __init__(self):
+        super().__init__(False, False)
 
 class NonBloquant(Objet):
-	def __init__(self):
-		super().__init__(False, True)
+    def __init__(self):
+        super().__init__(False, True)
